54008f29bc401f48631e765b7f0db8fe2f2f01ec
NishanthSrikar <23501A1218@pvpsit.ac.in>
2026-01-18 21:04:34 +0530

    Trial again

--- my-app/vite.config.js
+++ my-app/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/DevOps',
+  base: '/DevOps/',
   plugins: [
     react({
       babel: {
